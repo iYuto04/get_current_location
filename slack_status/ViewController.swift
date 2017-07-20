e62df67dd9160fc5f72cfaa548b419b8c04df0f1
Yuto Mizutani <i.yuto.curiosity@gmail.com>
2017-07-20 18:42:23 +0900

    initial commit

--- slack_status/ViewController.swift
+++ slack_status/ViewController.swift
@@ -7,19 +7,74 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+    @IBOutlet var latitudeLabel: UILabel!
+    @IBOutlet var longitudeLabel: UILabel!
+    var locationManager: CLLocationManager!
+    var latitude: Double = 0
+    var longitude: Double = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.locationManager = CLLocationManager()
+        self.locationManager.delegate = self
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func getLocation(_ sender: Any) {
+        self.locationManager.requestLocation()
+        self.longitudeLabel.text = String(self.longitude)
+        self.latitudeLabel.text = String(self.latitude)
+    }
 
 }
 
+extension ViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus){
+        switch status {
+        case .notDetermined:
+            print("ユーザーはこのアプリケーションに関してまだ選択を行っていません")
+            self.locationManager.requestWhenInUseAuthorization()
+            break
+        case .denied:
+            print("ローケーションサービスの設定が「無効」になっています (ユーザーによって、明示的に拒否されています）")
+            // 「設定 > プライバシー > 位置情報サービス で、位置情報サービスの利用を許可して下さい」を表示する
+            break
+        case .restricted:
+            print("このアプリケーションは位置情報サービスを使用できません(ユーザによって拒否されたわけではありません)")
+            // 「このアプリは、位置情報を取得できないために、正常に動作できません」を表示する
+            break
+        case .authorizedAlways:
+            print("常時、位置情報の取得が許可されています。")
+            // 位置情報取得の開始処理
+            break
+        case .authorizedWhenInUse:
+            print("起動時のみ、位置情報の取得が許可されています。")
+            //            self.locationManager.startUpdatingLocation()
+            // 位置情報取得の開始処理
+            break
+        }
+ 
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        for location in locations {
+            print("緯度:\(location.coordinate.latitude) 経度:\(location.coordinate.longitude) 取得時刻:\(location.timestamp.description)")
+            self.latitude = Double(location.coordinate.latitude)
+            self.longitude = Double(location.coordinate.longitude)
+            print("latitude is ", Double(location.coordinate.latitude))
+            print("longitude is ", Double(location.coordinate.longitude))
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("位置情報の取得に失敗しました")
+    }
+
+}
